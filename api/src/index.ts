1ece39dc1c8e1b24de6647b373deca4a7c6a2955
Supachai <supachai@outlook.com>
2026-01-17 11:43:00 +0700

    Add list templates api

--- api/src/index.ts
+++ api/src/index.ts
@@ -4,11 +4,16 @@ import templates from './routes/templates'
 
 const app = new OpenAPIHono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+// v1 Router
+const v1 = new OpenAPIHono()
+v1.route('/templates', templates)
 
-app.route('/templates', templates)
+// Mount v1
+app.route('/v1', v1)
+
+app.get('/', (c) => {
+  return c.text('RestInvoice API')
+})
 
 // OpenAPI Documentation
 app.doc('/doc', {
